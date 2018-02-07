0b61ce9c26b4360f897d003ca5855ef83ad467f5
victorchu8 <victorchu1996@gmail.com>
2018-02-06 22:18:32 -0700

    Segue from League to Skill added

---
+++ App-Swoosh/App-Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,35 @@
+//
+//  LeagueVC.swift
+//  App-Swoosh
+//
+//  Created by VICTOR CHU on 2018-02-06.
+//  Copyright © 2018 Victor Chu. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
